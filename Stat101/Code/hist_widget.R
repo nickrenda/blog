cc68801773d21d15e0ca9dd9b26418b79fa97305
epwalsh <epwalsh10@gmail.com>
2015-01-29 12:43:35 -0600

    a

---
+++ Stat101/Code/hist_widget.R
@@ -0,0 +1,34 @@
+toolTipFunc <- function(value) {
+  function(y) {
+    if (is.null(y)) return(NULL)
+    else {
+      length(value[(value >= y$xmin_) & (value < y$xmax_)])
+    }
+  }
+}
+
+widget <- function(data) {
+  shinyApp(
+    
+    ui = fluidPage(
+      
+      fluidRow(column(8,
+        ggvisOutput("plot"))),
+      fluidRow(column(8,
+        sliderInput("binwidth_adjust", "Binwidth adjustment", 0.2, 2, value=1.5, step=0.1)))
+    ),
+    
+    server = function(input, output) {
+      
+      gv <- reactive({
+        df <- data.frame(data)
+        #binwidth <- input_slider(0, 2, value = 1, step = 0.1)
+        binwidth <- input$binwidth_adjust
+        df %>% ggvis(~data) %>% 
+          add_tooltip(toolTipFunc(df$data), "hover") %>% 
+          layer_histograms(width = binwidth)
+      })
+      
+      gv %>% bind_shiny("plot")  }
+    )
+}
